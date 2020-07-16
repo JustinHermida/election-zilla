8d53927d3b500265fc78a57aa6742989c719505a
Jay Grissom <jay_grissom@intuit.com>
2020-07-16 10:42:34 -0700

    Added AppStore.js

---
+++ src/stores/appStore.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import {appReducer} from "../reducers/electionReducers";
+
+export const appStore = createStore(
+    appReducer,
+    composeWithDevTools(applyMiddleware(thunk)),
+);
