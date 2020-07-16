4f4b9161548dfb787f375843be20e4d3c266a05d
jhermida <justin_hermida@intuit.com>
2020-07-16 10:14:52 -0700

    initial election container, with new elections componenent

---
+++ src/components/NewElectionsPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { ElectionsContainer } from "../containers/ElectionsContainer";
+
+export const NewElectionsPage = () => {
+
+    return <ElectionsContainer display="new" />
+};
