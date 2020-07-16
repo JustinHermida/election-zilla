e98c600bcf080388d81ad37c4b11e612906b48a6
jhermida <justin_hermida@intuit.com>
2020-07-16 12:42:15 -0700

    Add questions to redux

--- src/reducers/electionReducers.js
+++ src/reducers/electionReducers.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import {ADD_BALLOT_QUESTION_ACTION, CLEAR_BALLOT_QUESTIONS_ACTION} from "../actions/electionActions";
 
 export const voterReducer = (voters = [], action) => {
     // Placeholder
@@ -10,7 +11,25 @@ export const electionReducer = (elections = [], action) => {
     return[];
 };
 
+export const ballotQuestionsReducer = (questions = [], action) => {
+  if(action.type === ADD_BALLOT_QUESTION_ACTION) {
+      return action.ballotQuestions;
+  }
+
+  return questions;
+};
+
+export const clearBallotQuestionsReducer = (questions = [], action) => {
+    if(action.type === CLEAR_BALLOT_QUESTIONS_ACTION) {
+        return action.ballotQuestions;
+    }
+
+    return questions;
+};
+
 export const appReducer = combineReducers({
     voters: voterReducer,
     elections: electionReducer,
+    ballotQuestions: ballotQuestionsReducer,
+    clearBallotQuestions: clearBallotQuestionsReducer,
 });
