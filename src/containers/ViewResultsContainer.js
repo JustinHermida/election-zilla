bd62a6e4085baa63583bcd5c7c44eecb5ac43959
jhermida <justin_hermida@intuit.com>
2020-07-16 17:32:23 -0700

    Add results button in view results

--- src/containers/ViewResultsContainer.js
+++ src/containers/ViewResultsContainer.js
@@ -2,13 +2,14 @@ import React, {useEffect, useMemo} from "react";
 import {ViewElectionResults} from "../components/ViewElectionResults";
 import {useDispatch, useSelector} from "react-redux";
 import {bindActionCreators} from "redux";
-import {refreshElections} from "../actions/electionActions";
+import {refreshElections, addViewResultsAction} from "../actions/electionActions";
 
 export const ViewResultsContainer = () => {
     const stateProps = useSelector(state => state);
     const dispatch = useDispatch();
-
+    
     const dispatchProps = useMemo(() => bindActionCreators({
+        onViewResults: addViewResultsAction,
         onRefreshElections: refreshElections,
     }, dispatch), [ dispatch ]);
 
@@ -16,5 +17,8 @@ export const ViewResultsContainer = () => {
         dispatchProps.onRefreshElections();
     }, [ dispatchProps ]);
 
-    return <ViewElectionResults elections={stateProps.elections}/>
+    return <ViewElectionResults
+        elections={stateProps.elections}
+        displayIds={stateProps.viewResults}
+        {...dispatchProps} />
 };
