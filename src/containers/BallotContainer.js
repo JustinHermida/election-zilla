e1237ecf1f8468337a9919757426e06b2d2ee063
Jay Grissom <jay_grissom@intuit.com>
2020-07-16 17:20:10 -0700

    Added Ballot Container.

---
+++ src/containers/BallotContainer.js
@@ -0,0 +1,25 @@
+import React, {useEffect, useMemo} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {bindActionCreators} from "redux";
+import {
+    refreshElections,
+} from "../actions/electionActions";
+
+import {EditBallot} from '../components/EditBallot'
+
+
+export const BallotContainer = () => {
+    const stateProps = useSelector(state => state);
+    const dispatch = useDispatch();
+
+    const dispatchProps = useMemo(() => bindActionCreators({
+        onRefreshElections: refreshElections,
+    }, dispatch), [ dispatch ]);
+
+    useEffect(() => {
+        dispatchProps.onRefreshElections();
+    }, [ dispatchProps ]);
+
+    return <EditBallot {...dispatchProps} {...stateProps} />;
+
+};
