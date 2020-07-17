49b5b5da68525d32886293333afcf3165a29ab17
Abhinandan Jayanth <Abhinandan_Jayanth@intuit.com>
2020-07-16 21:10:07 -0700

    Code cleanup

--- src/stores/voterToolStore.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-
-// import { voterReducer } from '../reducers/voterToolReducers';
-
-// export const voterToolStore = createStore(voterReducer, composeWithDevTools(applyMiddleware(thunk)));
